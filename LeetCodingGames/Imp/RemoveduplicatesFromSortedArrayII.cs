53456e41b10e7d611fa6b90beeb376807174a658
lifesteal <166237187+llfesteal@users.noreply.github.com>
2025-02-10 21:22:06 +0100

    Add Remove-duplicates-from-sorted-array-II https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/

---
+++ LeetCodingGames/Imp/RemoveduplicatesFromSortedArrayII.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodingGames.Imp
+{
+    public class RemoveduplicatesFromSortedArrayII
+    {
+        // TODO: Improve the execution time : 122ms
+        public int RemoveDuplicates(int[] nums)
+        {
+            int remainingItems = 0;
+            var sequenceCount = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int currentElement = nums[i];
+                bool alreadySeen = sequenceCount.TryGetValue(currentElement, out int countNumber);
+
+                if (!alreadySeen || countNumber < 2)
+                {
+                    nums[remainingItems] = currentElement;
+                    ++remainingItems;
+
+                    if (alreadySeen)
+                    {
+                        sequenceCount[currentElement] += 1;
+                    }
+                    else
+                    {
+                        sequenceCount.Add(currentElement, 1);
+                    }
+
+                }
+            }
+
+            return remainingItems;
+        }
+    }
+}
---
+++ LeetCodingGames/Test/RemoveduplicatesFromSortedArrayIITest .cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using LeetCodingGames.Imp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCodingGames.Test
+{
+    [TestClass]
+    public class RemoveduplicatesFromSortedArrayTestII
+    {
+        [TestMethod]
+        public void RemoveElementShouldRemoveDuplicated_Case1()
+        {
+            // Arrange
+            int[] nums = new[] { 1, 1, 1, 2, 2, 3 };
+            var s = new RemoveduplicatesFromSortedArrayII();
+
+            // Act
+            int result = s.RemoveDuplicates(nums);
+
+            // Assert
+            result.Should().Be(5);
+            int[] expectedNums = new[] { 1, 1, 2, 2, 3 };
+            nums.Take(5).Should().BeEquivalentTo(expectedNums);
+        }
+
+        [TestMethod]
+        public void RemoveElementShouldRemoveDuplicated_Case2()
+        {
+            // Arrange
+            int[] nums = new[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+            var s = new RemoveduplicatesFromSortedArrayII();
+
+            // Act
+            int result = s.RemoveDuplicates(nums);
+
+            // Assert
+            result.Should().Be(7);
+            int[] expectedNums = new[] { 0, 0, 1, 1, 2, 3, 3 };
+            nums.Take(7).Should().BeEquivalentTo(expectedNums);
+        }
+    }
+}
