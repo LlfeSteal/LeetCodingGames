e36594d6df277377257d4419322a884473fdea28
lifesteal <166237187+llfesteal@users.noreply.github.com>
2025-02-10 21:33:53 +0100

    Add Remove-element https://leetcode.com/problems/remove-element/

---
+++ LeetCodingGames/Imp/RemoveElements/RemoveElementsFromArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodingGames.Imp.RemoveElements
+{
+    public class RemoveElementsFromArray
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int remainingItems = 0;
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int currentElement = nums[i];
+
+                if (currentElement != val)
+                {
+                    nums[remainingItems] = currentElement;
+                    ++remainingItems;
+                }
+            }
+
+            return remainingItems;
+        }
+    }
+}
---
+++ LeetCodingGames/Test/RemoveElements/RemoveElementsFromArrayTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using LeetCodingGames.Imp.RemoveElements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCodingGames.Test.RemoveElements
+{
+    [TestClass]
+    public class RemoveElementsFromArrayTest
+    {
+        [TestMethod]
+        public void RemoveElementShouldRemoveDuplicated_Case1()
+        {
+            // Arrange
+            int[] nums = new[] { 3, 2, 2, 3 };
+            var s = new RemoveElementsFromArray();
+
+            // Act
+            int result = s.RemoveElement(nums, 3);
+
+            // Assert
+            result.Should().Be(2);
+            int[] expectedNums = new[] { 2, 2 };
+            nums.Take(2).Should().BeEquivalentTo(expectedNums);
+        }
+
+        [TestMethod]
+        public void RemoveElementShouldRemoveDuplicated_Case2()
+        {
+            // Arrange
+            int[] nums = new[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            var s = new RemoveElementsFromArray();
+
+            // Act
+            int result = s.RemoveElement(nums, 2);
+
+            // Assert
+            result.Should().Be(5);
+            int[] expectedNums = new[] { 0, 1, 3, 0, 4 };
+            nums.Take(5).Should().BeEquivalentTo(expectedNums);
+        }
+    }
+}
