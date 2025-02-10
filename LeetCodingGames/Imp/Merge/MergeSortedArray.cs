03316318aa1e45ea63493726ce7c46417862f7cc
lifesteal <166237187+llfesteal@users.noreply.github.com>
2025-02-10 21:46:26 +0100

    Add Merge-sorted-array https://leetcode.com/problems/merge-sorted-array/

---
+++ LeetCodingGames/Imp/Merge/MergeSortedArray.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodingGames.Imp.Merge
+{
+    public class MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int totalSize = nums1.Length;
+            int currentNums1Index = 0;
+            int currentNums2Index = 0;
+            int[] mergedArray = new int[totalSize];
+
+            for (int i = 0; i < totalSize; ++i)
+            {
+                if (currentNums1Index == m)
+                {
+                    for (int j = currentNums2Index; j < nums2.Length; ++j)
+                    {
+                        mergedArray[i++] = nums2[j];
+                    }
+                    break;
+                }
+                if (currentNums2Index == nums2.Length)
+                {
+                    for (int j = currentNums1Index; j < m; ++j)
+                    {
+                        mergedArray[i++] = nums1[j];
+                    }
+                    break;
+                }
+
+                int currentNums1 = nums1[currentNums1Index];
+                int currentNums2 = nums2[currentNums2Index];
+
+
+                if (currentNums1 <= currentNums2)
+                {
+                    mergedArray[i] = currentNums1;
+                    ++currentNums1Index;
+                }
+                else
+                {
+                    mergedArray[i] = currentNums2;
+                    ++currentNums2Index;
+                }
+            }
+
+            for (int i = 0; i < m + n; i++)
+            {
+                nums1[i] = mergedArray[i];
+            }
+        }
+    }
+}
---
+++ LeetCodingGames/Test/Merge/MergeSortedArrayTest.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using LeetCodingGames.Imp.Merge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCodingGames.Test.Merge
+{
+    [TestClass]
+    public class MergeSortedArrayTest
+    {
+        [TestMethod]
+        public void MergeShouldMergeArray_Case1()
+        {
+            // Arrange
+            int[] nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+            int[] nums2 = new[] { 2, 5, 6 };
+            var s = new MergeSortedArray();
+
+            // Act
+            s.Merge(nums1, 3, nums2, 3);
+
+            // Assert
+            int[] expectedNums1 = new[] { 1, 2, 2, 3, 5, 6 };
+            nums1.Should().BeEquivalentTo(expectedNums1);
+        }
+
+        [TestMethod]
+        public void MergeShouldMergeArray_Case2()
+        {
+            // Arrange
+            int[] nums1 = new[] { 1 };
+            int[] nums2 = new int[0];
+            var s = new MergeSortedArray();
+
+            // Act
+            s.Merge(nums1, 1, nums2, 0);
+
+            // Assert
+            int[] expectedNums1 = new[] { 1 };
+            nums1.Should().BeEquivalentTo(expectedNums1);
+        }
+
+        [TestMethod]
+        public void MergeShouldMergeArray_Case3()
+        {
+            // Arrange
+            int[] nums1 = new int[] { 0 };
+            int[] nums2 = new[] { 1 };
+            var s = new MergeSortedArray();
+
+            // Act
+            s.Merge(nums1, 0, nums2, 1);
+
+            // Assert
+            int[] expectedNums1 = new[] { 1 };
+            nums1.Should().BeEquivalentTo(expectedNums1);
+        }
+    }
+}
